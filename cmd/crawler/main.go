521e533555a9ff8fb52582d0d1f3c621f44fcc25
Nikita Lopukhin <nalopukhin@avito.ru>
2025-03-29 00:40:30 +0300

    hw5 even better#2

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -1,32 +1,18 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/chzyer/readline"
 	"github.com/fatih/color"
 
 	"search/internal/index"
-	"search/internal/pipe"
 	"search/internal/pkg"
-	"search/internal/task"
 )
 
 const (
-	crawlerTimeout = time.Minute
-
-	linksChanCapacity    = 100
-	pipelineChanCapacity = 100
-
-	numParallelFetchers = 20
-
-	numDocumentWords = 1000
-	numDocuments     = 200
-
 	processedDocumentsDirPath = "./data/5/processed"
 	rawDocumentsDirPath       = "./data/5/raw"
 	indexPath                 = "index5.json"
@@ -231,49 +217,4 @@ func shouldHighlight(word, queryWord string) bool {
 	return word == queryWord ||
 		(len(queryWord) > 3 && strings.Contains(word, queryWord)) ||
 		(len(word) > 3 && strings.Contains(queryWord, word))
-}
-
-func fetch() {
-	ctx, cancel := context.WithTimeout(context.Background(), crawlerTimeout)
-	defer cancel()
-
-	links := []string{
-		"https://ru.wikipedia.org/wiki/Авито",
-		"https://ru.wikipedia.org/wiki/Французский_язык",
-	}
-
-	linksChan := make(chan string, linksChanCapacity)
-	for _, link := range links {
-		linksChan <- link
-	}
-
-	<-pipe.StartPipeline(ctx,
-		task.NewTaskGenerator(pipelineChanCapacity, linksChan),
-
-		pipe.Parallelize(
-			pipe.NewPipe(task.FetchHandler), numParallelFetchers,
-		),
-		pipe.NewPipe(task.ParseHandler),
-
-		pipe.Satisfies(task.NewDocumentSizeFilter(numDocumentWords)),
-		pipe.Satisfies(task.CyrillicFilter),
-		pipe.Satisfies(task.NewDocumentCounterFilter(numDocuments, cancel)),
-		pipe.Satisfies(task.NotFinishedFilter),
-
-		pipe.NewPipe(task.ProcessDocumentHandler),
-
-		pipe.Synchronize(
-			pipe.NewAsyncPipe(task.NewFeedLinksAsyncHandler(linksChan)),
-		),
-
-		pipe.Synchronize(
-			task.NewIndexerPipe(indexPath),
-		),
-		pipe.NewPipe(task.NewSaveHandler(processedDocumentsDirPath, rawDocumentsDirPath)),
-
-		pipe.Synchronize(
-			pipe.NewAsyncPipe(task.NewLogAsyncHandler()),
-		),
-		pipe.NewWaitPipe[*task.Task](),
-	)
 }
